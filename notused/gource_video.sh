7633499422a395a75f37ff92ec298498865272c3
1138-4EB <1138-4EB@users.noreply.github.com>
2018-06-18 15:57:27 +0100

    add script to generate a video with gource

---
+++ notused/gource_video.sh
@@ -0,0 +1,2 @@
+gource --key -1920x1080 --seconds-per-day .05 --auto-skip-seconds .01 --date-format "%F" --font-colour FFFFFF --hide bloom,filenames --hash-seed 128 --file-idle-time 0 -o gource.ppm
+ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i gource.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
